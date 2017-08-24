9daf684e590b16ead0ed037248519664027a68e1
jatfret <jatfret@gmail.com>
2017-08-24 22:36:38 +0800

    delete unused constants

--- src/DatePicker/src/constants/index.js
+++ src/DatePicker/src/constants/index.js
@@ -10,12 +10,7 @@ const YearConstants = {
   YEAR_COL_COUNT: 3,
   YEAR_ROW_COUNT: 4,
 }
-const OPTIONS = {
-  mode: 'date', //month, year 时间选择面板
-  isSection: false, //是否是截面
-  isRange: false, //是否是时间段选择
-}
 
 const DefaultFormat = "YYYY.MM.DD";
 
-export { DateConstants, MonthConstants, YearConstants, OPTIONS, DefaultFormat } ;
+export { DateConstants, MonthConstants, YearConstants, DefaultFormat } ;
